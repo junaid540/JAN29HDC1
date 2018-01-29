44887472597b78a394888169717818addb433fc8
k.hussain.syed <k.hussain.syed@HDC1-L-66XHD02.dir.svc.accenture.com>
2018-01-29 15:07:41 +0530

    Merge from Eclipse

---
+++ FirstProject/src/main/java/SampleProjectJan29/FirstProject/App.java
@@ -0,0 +1,13 @@
+package SampleProjectJan29.FirstProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
